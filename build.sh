0c2675785e699e9cab09e856165410b6231b6140
kamal <kamal@reaper.(none)>
2010-12-01 07:15:29 +0800

    snapshot

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-git clone git@github.com:k4ml/devserver.git
 drush dl --drupal-project-rename=www drupal
 cp www/sites/default/default.settings.php www/sites/default/settings.php
-drush dl admin_menu
-drush dl devel
+mkdir www/sites/all/modules
+mkdir www/sites/all/themes
+
+drush dl admin_menu && mv admin_menu www/sites/all/modules
+drush dl devel && mv devel www/sites/all/modules
+
+git clone git@github.com:k4ml/devserver.git server
+git clone git@github.com:k4ml/drupal-qb.git www/sites/all/modules/qb
